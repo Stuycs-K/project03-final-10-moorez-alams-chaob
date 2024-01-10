e4db44a96e873cb9f16831e3fddb61e8dd5b6631
zac <zmoore40@stuy.edu>
2024-01-10 15:35:07 -0500

    worked on readData

--- playerData.c
+++ playerData.c
@@ -6,26 +6,30 @@ void writeData (int numChips, int roundNumber, char playerInitials[]) {
     int dataStructSize = sizeof(struct playerData);
     data = malloc(dataStructSize);
     data -> numChips = numChips;
+    data -> roundNumber = roundNumber;
     strcpy(data -> playerInitials, playerInitials);
     // open file, write
     FILE* w_file = fopen("data.txt", "a");
-    fwrite(data, sizeof(data), 1, w_file);
+    fwrite(data, dataStructSize, 1, w_file);
     fclose(w_file);
 }
 
-// void readData () 
-// {
-//     int dataStructSize = sizeof(struct playerData);
-//     int r_file = open("data.txt", O_RDONLY, 0);
-//     struct stat s;
-//     stat("data.txt", &s)
-//     int numStructs = s.st_size / dataStructSize;
-//     read(w_file, data, dataStructSize);
-// }
+void readData () {
+    int dataStructSize = sizeof(struct playerData);
+    FILE* r_file = fopen("data.txt", "r");
+    struct stat s;
+    struct playerData *data;
+    stat("data.txt", &s);
+    // int numStructs = s.st_size / dataStructSize;
+    // printf("%d", numStructs);
+    fread(data, dataStructSize, 1, r_file);
+    printf("%d\n", data -> numChips);
+}
 
 
 
 int main () {
     char i[2] = "ZM";
-    writeData(1, 1, i);
+    writeData(4, 1, i);
+    readData();
 }
